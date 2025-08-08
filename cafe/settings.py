55dbad474ee016171fbe684fc46c7595adb9c04e
Asrifa-hassan <achiachu1122@gmail.com>
2025-08-08 19:06:39 +0530

    hosting

--- cafe/settings.py
+++ cafe/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-cn3i-2xz8h(tf-loqs7^%#vg)eoa@udc8j%8)vw-hxklnr__s!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://coffee-rnvk.onrender.com']
 
 
 # Application definition
