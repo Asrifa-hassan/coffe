a806430273af6f37e82ac9c5c7b06daf77c68795
Asrifa-hassan <achiachu1122@gmail.com>
2025-08-08 22:40:51 +0530

    hosting

--- cafe/settings.py
+++ cafe/settings.py
@@ -134,6 +134,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATICFILES_DIRS = [STATIC_DIR,]
 
+LOGIN_URL = '/login_page/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
